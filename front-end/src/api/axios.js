49ba4484297b62d155f538d16cd59a006a221fea
vannovich <awontuvannovich@gmail.com>
2026-04-26 11:48:45 +0100

    Fixes

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_URL_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_URL_URL || "https://employee-management-system-server-mauve.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
